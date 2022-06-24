4040e717d26337f97cc7d0d267c1a9b37b82a7fa
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-06-24 19:48:48 +0300

    Update AccessValue.cpp

--- Arrays/AccessValue.cpp
+++ Arrays/AccessValue.cpp
@@ -1,15 +1,11 @@
-void AccessValue(int* Arr, int Size, int Value)
+int AccessValue(int* Arr, int Size, int Value)
 {
-    bool Found = false;
     for(int i=0;i<Size;i++)
     {
         if(Arr[i] == Value)
         {
-            Found == true;
-            cout << "Found At index " << i << endl;
-            return;
+            return i;
         }
-        else Found == false;
     }
-    cout <<"Not Found"<< endl;
+     return -1;
 }
