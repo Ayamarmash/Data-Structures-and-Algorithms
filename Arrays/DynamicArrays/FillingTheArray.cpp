8d49867aca1f19817f9777a867c69f6e9d1036a7
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-06-24 18:36:24 +0300

    Update FillingTheArray.cpp

--- Arrays/DynamicArrays/FillingTheArray.cpp
+++ Arrays/DynamicArrays/FillingTheArray.cpp
@@ -1,7 +1,7 @@
-void FillDynamicArray(int *Arr) //pass the array by address 
+void FillArray(int *Arr, int Size)
 {
-    for(int i=0; i<sizeof(Arr); i++) // sizeof Takes the Arr as a parameter and return the size of it
+    for(int i=0; i<Size; i++)
     {
-        Arr[i] = (rand() % 10) + 1; // Filling the array with random numbers between (1 , 10)
+        Arr[i] = (rand() % 10) + 1;
     }
 }
