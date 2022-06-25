9329f340ed94d8a8043586cbae7643e36daf2fea
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-06-25 22:20:33 +0300

    Create hashFunction.cpp

---
+++ HashMap/hashFunction.cpp
@@ -0,0 +1,6 @@
+    // hash function to map values to key
+///////////This hash function could be implemented in an another way,, you can choose how to evaluate the index
+    int hashFunction(int x) 
+    {
+        return (x % Size);
+    }
