0ca805ab7a291f153167c546ac8c53a9a8a154f5
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-03 01:40:34 +0300

    Update Structure.cpp

--- Graph/Structure.cpp
+++ Graph/Structure.cpp
@@ -1,15 +1,21 @@
 class Graph
 {
     private:
-    int v; //Vertices
-    int e; //Edges
-    int **adjMatrix;
+    int** adjMatrix ;
+    int nodes ;
+    int edges;
     
     public:
-    Graph(int n , int m)
+    Graph(int x, int edg)
     {
-        v = n;
-        e = m;
-        adjMatrix = new int[v+1][v+1];
+        nodes = x;
+        edges = edg;
+        
+        adjMatrix = new int*[x];
+        
+        for(int i=0;i<x;i++)
+        {
+            adjMatrix[i] = new int[x];
+        }
     }
 }
