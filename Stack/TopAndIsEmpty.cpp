47c80a11f5a30ca04514e5269897e082729ebd3f
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-02 20:23:00 +0300

    Create TopAndIsEmpty.cpp

---
+++ Stack/TopAndIsEmpty.cpp
@@ -0,0 +1,12 @@
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else return true;
+    }
+    int Top()
+    {
+        return arr[top];
+    }
