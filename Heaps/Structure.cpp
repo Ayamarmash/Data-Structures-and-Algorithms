1db59212776020506d8ff14af340a3f3cb8afcfc
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-04 13:38:46 +0300

    Update Structure.cpp

--- Heaps/Structure.cpp
+++ Heaps/Structure.cpp
@@ -1,1 +1,11 @@
-  
+ class Heap
+{
+        int *v; // array to store the data
+        int size;
+public:
+        // constructor
+        Heap()
+        {
+            size = 0 ;
+            v = new int[size];
+        }
