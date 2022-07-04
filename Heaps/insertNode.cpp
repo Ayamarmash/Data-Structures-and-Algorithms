d8cd1d3b69d39b36d91f19e8e6451a601d561668
Ayamarmash <58006991+Ayamarmash@users.noreply.github.com>
2022-07-04 13:28:48 +0300

    Update insertNode.cpp

--- Heaps/insertNode.cpp
+++ Heaps/insertNode.cpp
@@ -1,1 +1,37 @@
-
+        void insertItem(int x)
+        {
+            v[size++] = x;
+            heapify(v,size, size-1);
+            cout << x << " Added" << endl;
+        }
+void heapify(int a[], int n, int i)
+{	
+    int parent = i;
+	if(i<n)
+	{
+		//finding left subtree of parent
+		int left = 2*i+1; 
+		//finding Right subtree of parent
+		int right = 2*i+2; 
+		//is children index is valid or not
+		if(left<n || right<n) 
+		{
+			//checking whether parent is less than children or not
+			if(a[parent]<a[left] || a[parent]<a[right])
+			{
+                                //finding the largest children
+				if(a[left]<a[right]) 
+				{
+                    //swaping the parent with largest children
+					swap(a[right],a[parent]); 
+					heapify(a,n,right);  //Recursively heapify the heap
+				}	
+				else
+				{
+					swap(a[left],a[parent]);
+					heapify(a,n,right);
+				}
+			}
+		}
+	}
+}
